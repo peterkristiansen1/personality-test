1f29e19cffd1890067338251d10d60fec767422f
Peter Kristiansen <pkr@revealit.dk>
2023-05-16 15:28:20 +0200

    Styling score symbols and submit button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,15 @@ interface Item {
   choices: Choice[];
 }
 
+export const extraversionDomain = "E";
+
 export default function Home() {
-  const items: Item[] = getItems();
-  const getImageUrl = (query) =>
+  const items: Item[] = getItems()
+    .filter((item) => item.domain === extraversionDomain)
+    .slice(0, 2);
+  const getImageUrl = (query: string) =>
     `https://source.unsplash.com/random?${query.replace(/\s/g, "-")}`;
+  const scoreSymbols = ["👎👎", "👎", "👎👍", "👍", "👍👍"];
   return (
     <main className={styles.main}>
       <header className={styles.header}>
@@ -35,14 +40,19 @@ export default function Home() {
                 <span className={styles.number}>{item.num}</span>
                 <span className={styles.text}>{item.text}</span>
               </legend>
-              <img src={getImageUrl(item.text)} className={styles.image} />
+              <img
+                src={getImageUrl(item.text)}
+                className={styles.image}
+                loading={item.num < 1 ? "eager" : "lazy"}
+                alt=""
+              />
               {item.choices.map((choice) => (
                 <div key={choice.score} className={styles.choice}>
                   {/* TODO: Make input required */}
                   <input
                     type="radio"
                     id={`${item.num}-${choice.score}`}
-                    name={`${item.domain}${item.facet}`}
+                    name={item.facet}
                     value={choice.score}
                     className={styles.radioInput}
                   />
@@ -50,7 +60,10 @@ export default function Home() {
                     htmlFor={`${item.num}-${choice.score}`}
                     className={styles.choiceText}
                   >
-                    {choice.text}
+                    <span className={styles.scoreSymbols}>
+                      {scoreSymbols[choice.score - 1]}
+                    </span>
+                    <span className={styles.choiceText}>{choice.text}</span>
                   </label>
                 </div>
               ))}
@@ -58,8 +71,24 @@ export default function Home() {
           </fieldset>
         ))}
         <div className={styles.submitWrapper}>
-          You've reached the end.
-          <button type="submit">Show result</button>
+          <button type="submit" className={styles.submitButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className={styles.crownIcon}
+              viewBox="0 0 24 24"
+            >
+              <path d="M2 4l3 12h14l3-12-6 7-4-7-4 7-6-7zm3 16h14"></path>
+            </svg>
+            <span>You made it!</span>
+            <span>Tap to show result</span>
+          </button>
         </div>
       </form>
     </main>
